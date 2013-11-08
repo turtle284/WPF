84e5af37546caa49eba515218b80ad5c391319a1
Steve <smsway84@fullsail.edu>
2013-11-07 23:09:54 -0500

    Commited the steps to the calculation

---
+++ Expressions_Personal/js/script.js
@@ -0,0 +1,31 @@
+//Steven Swayngim 11/4/2013 Personal Expressions
+
+//Calculate your monthly expenses
+
+//Prompt asking for electric bill
+
+
+//Prompt asking for gas bill
+
+
+//Prompt asking for Monthly Rent
+
+
+//Prompt asking for cable bill
+
+
+//Prompt asking asking for grocery bill
+
+
+//How many people reside in your household
+
+
+//Sum of the total expenses
+
+
+//Divide the total by the number of residents to get each share
+
+
+//Print out results to console
+
+
