c05aee4ec17f34691055aedee0c2522f0c7289da
Steve <smsway84@fullsail.edu>
2013-11-07 23:11:45 -0500

    Added the code for the calucaltion and console log

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -18,14 +18,15 @@ var cable = prompt('What is your monthly cable bill?');
 var grocery = prompt('What is your monthly grocery bill?');
 
 //How many people reside in your household
-
+var residents = prompt('How many residents are in your household?');
 
 //Sum of the total expenses
-
+var total = electric + gas + rent + cable + grocery;
 
 //Divide the total by the number of residents to get each share
-
+var share = total / residents;
 
 //Print out results to console
+console.log('Your hose spent a total of $' + total + '.  Your share is $' + share + '.');
 
 
