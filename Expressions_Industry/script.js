5b711f40a4553430c45e319b7503fffefff336d5
Steve <smsway84@fullsail.edu>
2013-11-07 23:14:39 -0500

    Commited the steps to the calculation

---
+++ Expressions_Industry/script.js
@@ -0,0 +1,18 @@
+//Steve Swayngim 11/4/2013 Industry Expression
+
+//Calculate how many oil drums you need to order for the month
+
+//Prompt asking how many vehicles are in your fleet?
+
+//Prompt asking how many barrels of oil are currently instock.
+
+//Each barrel hold 10gallons.  You need to convert that to Quarts
+
+//Calculate the total amount of oil needed for the fleet (in quarts).  Trucks use 40qtrs reefers use 35.
+
+//Subtract the amount of oil you currently have
+
+//Determine the amount of barrels needed
+
+
+//Print out results to the console
