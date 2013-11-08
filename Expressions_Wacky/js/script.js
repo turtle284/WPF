1296d89672642df08e4353e5af82c95c744c5667
Steve <smsway84@fullsail.edu>
2013-11-07 23:27:13 -0500

    Added the commits for the steps

---
+++ Expressions_Wacky/js/script.js
@@ -0,0 +1,22 @@
+// Steven Swayngim 11/4/2013 Expressions Worksheet
+
+//What is the lowest amount of packs of hotdogs and buns needed to run out at the same time.
+
+//Prompt asking how many adults there are. Adults eat a hot dog and a bun
+
+//Prompt asking how many kids there are.  Kids eat a hot dog with no bun.
+
+
+//Prompt asking how many dogs.  Dogs eat hot dog with no bun.
+
+//Set the amount of people eating buns
+
+//Set the amount of people eating hotdogs
+
+//Find out the lowest amount of packs of buns needed to use up the packs with no extras
+
+//Find out the lowest amount of packs of hot dogs needed to use up the packs with no extras
+
+//Calculate the amount of buns used
+
+//Print results
