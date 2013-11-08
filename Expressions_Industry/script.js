c0c842def402daa7a4cbb97b074c1a604de90e0d
Steve <smsway84@fullsail.edu>
2013-11-07 23:16:17 -0500

    Added the codes to the prompts also added commits to enter an aray

--- Expressions_Industry/script.js
+++ Expressions_Industry/script.js
@@ -3,8 +3,12 @@
 //Calculate how many oil drums you need to order for the month
 
 //Prompt asking how many vehicles are in your fleet?
+var fleet = prompt('How many vehicles are in your fleet');
+
+//Create an array that will hold your fleet info
 
 //Prompt asking how many barrels of oil are currently instock.
+var barrels = prompt('How many barrels of oil do you currently have?');
 
 //Each barrel hold 10gallons.  You need to convert that to Quarts
 
