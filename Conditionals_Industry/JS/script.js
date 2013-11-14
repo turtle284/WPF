6eb8745729f0ff23be098024a2181bbab50b9869
Steve <smsway84@fullsail.edu>
2013-11-14 16:36:59 -0500

    Set the variables

--- Conditionals_Industry/JS/script.js
+++ Conditionals_Industry/JS/script.js
@@ -1,3 +1,8 @@
 //Steve Swayngim 11/13/2013 Conditionals Industry
 
 //Calculate the amount of payment you will receive for your accident`
+
+//set the variables
+var type ="";
+var damage = 0;
+var deductible = 0;
