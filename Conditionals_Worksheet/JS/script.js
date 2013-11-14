7e2696945959b5da2e06975db82c95fbd2cdb528
Steve <smsway84@fullsail.edu>
2013-11-14 14:11:31 -0500

    Entered the comments for the first problem

---
+++ Conditionals_Worksheet/JS/script.js
@@ -0,0 +1,13 @@
+//Steve Swayngim 11/9/13 Conditionals Worksheet
+
+//Celsius to Fahrenheit converter
+
+//Enter the givens
+
+//Separate the number from the unit of measurement
+
+//Print results to the console
+
+//The calculation if it is Fahrenheit
+
+//The calculation if it is Celsius
