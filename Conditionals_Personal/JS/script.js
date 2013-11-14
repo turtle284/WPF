effafd71da91a82082499d998fbcafd1b14b86e8
Steve <smsway84@fullsail.edu>
2013-11-14 14:48:47 -0500

    Entered the basic comments

---
+++ Conditionals_Personal/JS/script.js
@@ -0,0 +1,11 @@
+//Steve Swayngim 11/12/2013 Personal Conditionals
+
+//Given Enter the amount of the bill
+
+//Given Enter the amount of people in your party
+
+//Create a conditional to see if the party is greater than 6
+
+//divide the bill by the amount of people
+
+//Print results to console
