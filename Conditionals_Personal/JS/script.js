7b6276d908ad34d2bd4915bdc325281b760fc13a
Steve <smsway84@fullsail.edu>
2013-11-14 14:50:43 -0500

    Added additional comment instructions

--- Conditionals_Personal/JS/script.js
+++ Conditionals_Personal/JS/script.js
@@ -1,10 +1,16 @@
 //Steve Swayngim 11/12/2013 Personal Conditionals
 
+//define the variables
+
 //Given Enter the amount of the bill
 
 //Given Enter the amount of people in your party
 
 //Create a conditional to see if the party is greater than 6
+
+//If the party is greater than 6 tip is automatically 18 percent
+
+//If the party is less than 6 prompt asking for the tip amount
 
 //divide the bill by the amount of people
 
