07259414dac73c4fa5cee4aa409d9f479c82a956
Steve <smsway84@fullsail.edu>
2013-11-14 14:13:09 -0500

    Entered the givens and variables for the code

--- Conditionals_Worksheet/JS/script.js
+++ Conditionals_Worksheet/JS/script.js
@@ -3,8 +3,12 @@
 //Celsius to Fahrenheit converter
 
 //Enter the givens
+var temp = "100C";
 
 //Separate the number from the unit of measurement
+var length = temp.length;
+var degree = temp.slice((length-1),length);
+var unit = parseInt(temp.slice(0,(length-1)));
 
 //Print results to the console
 
