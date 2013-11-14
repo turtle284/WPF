69ded1de7b66d8cea45cbb4e89279559009b5a3e
Steve <smsway84@fullsail.edu>
2013-11-14 14:28:40 -0500

    Create the if statement to determine the letter grade

--- Conditionals_Worksheet/JS/script.js
+++ Conditionals_Worksheet/JS/script.js
@@ -53,5 +53,29 @@ var percent = 79;
 
 //Print results to the console
 //Determine the equivalent letter grade
+if (percent >= 95){
+	console.log('You have a ' + percent + '%, which means you have earned an A+ in the class!');
+}
+else if(percent < 95 && percent >=90){
+	console.log('You have a ' + percent + '%, which means you have earned an A in the class!');
+}
+else if(percent < 90 && percent >=85){
+	console.log('You have a ' + percent + '%, which means you have earned a B+ in the class!');
+}
+else if(percent < 85 && percent >=80){
+	console.log('You have a ' + percent + '%, which means you have earned a B in the class!');
+}
+else if(percent < 80 && percent >=76){
+	console.log('You have a ' + percent + '%, which means you have earned a C+ in the class!');
+}
+else if(percent < 76 && percent >=73){
+	console.log('You have a ' + percent + '%, which means you have earned a C in the class!');
+}
+else if(percent < 73 && percent >=70){
+	console.log('You have a ' + percent + '%, which means you have earned a D in the class!');
+}
+else {
+	console.log('You have a ' + percent + '%, which means you have earned a F in the class!');
+};
 
 	
