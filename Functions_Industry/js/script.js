6f7e8d9730fdaadcca33659708653321e38f7e4e
Steve <smsway84@fullsail.edu>
2013-11-22 00:10:12 -0500

    entered the prompts

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -5,3 +5,4 @@ var myCards = ['0000000000000000','1234567887654321','1111111111111111'];
 
 var checkCard ='';
 //enter the prompts
+var checkCard = prompt('Please enter your 16 digit card number.');
