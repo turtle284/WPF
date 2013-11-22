a83b0369334e13de52a657281f109cd79b04130a
Steve <smsway84@fullsail.edu>
2013-11-22 00:09:35 -0500

    entered the variables

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,3 +1,6 @@
 //Steve Swayngim 11/21/13 Functions Industry
 //check if they enter a valid credit card number
 //define the variables
+var myCards = ['0000000000000000','1234567887654321','1111111111111111'];
+
+var checkCard ='';
