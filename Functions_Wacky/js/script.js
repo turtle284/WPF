cd5363516b90abd6b28688022640ef971fc52ea7
Steve <smsway84@fullsail.edu>
2013-11-21 23:59:24 -0500

    entered the prompts

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -7,3 +7,8 @@ var bullets = 0;
 var gallons = 0;
 
 //enter prompts
+chips = prompt('How many bags of chips do you have?');
+
+bullets = prompt('How many bullets do you have?');
+
+gallons = prompt('How many gallons of water fo you have?');
